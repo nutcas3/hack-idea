d70867ae4b4d1b70476c30a91d649df261f11f8d
nutcas3 <mauricee423@gmail.com>
2024-04-30 01:39:13 +0300

    Configure admin interface for event, leaderboard, problem, testcase, and submission models
    - Created admin classes for Event, Leaderboard, Problem, Testcase, and Submission models to customize their appearance and functionality in the Django admin interface.
    - Added list display, filters, ordering, and search fields for each model to improve navigation and searchability.
    - Implemented custom actions for setting time limits on test cases and evaluating submissions asynchronously using Celery tasks.
    - Registered the admin classes with the Django admin site for easy management of the application data.

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,74 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Event, Leaderboard, Problem, Submission, Testcase
+from .tasks import set_tc_time_limits, submit
+
+
+class Event_Admin(admin.ModelAdmin):
+    list_display = ["title", "is_contest", "datetime", "duration"]
+    list_filter = ["is_contest"]
+    ordering = ["-datetime"]
+    search_fields = ["title"]
+
+class Leaderboard_Admin(admin.ModelAdmin):
+    list_display = ["event", "user", "score"]
+    list_filter = ["event", "user"]
+    ordering = ["-event__datetime", "-score"]
+    search_fields = ["user", "event"]
+
+class Problem_Admin(admin.ModelAdmin):
+    list_display = ["title", "event", "no_of_testcases"]
+    list_filter = ["event"]
+    ordering = ["-event__datetime"]
+    search_fields = ["title"]
+    actions = ["set_testcases_time_limits"]
+
+    def set_testcases_time_limits(self, request, queryset):
+        for problem in queryset:
+            set_tc_time_limits.delay(problem.id)
+
+class Testcase_Admin(admin.ModelAdmin):
+    list_filter = ["problem__title"]
+    search_fields = ["problem__title"]
+    ordering = ["-problem__event__datetime", "problem__slug"]
+    list_display = ["__str__", "tc_input_size", "tc_output_size", "std_time_limit"]
+    actions = ["set_tc_files_size", "set_testcases_time_limits"]
+
+    def set_tc_files_size(self, request, testcases):
+        for testcase in testcases:
+            testcase.save()
+
+    def set_testcases_time_limits(self, request, testcases):
+        problem_ids = {testcase.problem.id for testcase in testcases}
+        for problem_id in problem_ids:
+            set_tc_time_limits.delay(problem_id)
+
+class Submission_Admin(admin.ModelAdmin):
+    list_display = ["user", "status", "problem", "datetime"]
+    list_filter = ["problem__event", "problem"]
+    ordering = ["-datetime"]
+    search_fields = ["user", "problem", "problem__event"]
+    actions = ["evaluate"]
+
+    def evaluate(self, request, queryset):
+        queryset.order_by("datetime")
+        for submission in queryset:
+            problem = submission.problem
+            user = submission.user
+            submitted_solution = submission.solution
+            language_id = submission.language_id
+            submission_time = submission.datetime
+
+            submit.delay(
+                problem_id=problem.id,
+                user_id=user.id,
+                submission_id=submission.id,
+                solution=submitted_solution,
+                lang_id=language_id,
+                submitting_time=submission_time,
+            )
+admin.site.register(Event, Event_Admin)
+admin.site.register(Leaderboard, Leaderboard_Admin)
+admin.site.register(Problem, Problem_Admin)
+admin.site.register(Testcase, Testcase_Admin)
+admin.site.register(Submission, Submission_Admin)
